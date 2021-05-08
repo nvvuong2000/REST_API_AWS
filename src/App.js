49d00f8a1ba61a021eab9f1ce0405f9eb4f3a9a8
Vương Nguyễn <nguyenvuongs2000@gmail.com>
2021-05-08 13:43:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { useState, useEffect} from "react"
 
 function App() {
+  
+  const [products, setProduct] = useState([]);
+  
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        await axios.get('https://fbjlr2p7xi.execute-api.us-east-2.amazonaws.com/test/iots')
+        .then((res) => { setProduct(res.data) })
+      } catch (error) {
+        console.log('Failed to fetch posts: ', error.message);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {products.iots&&
+          products.iots.map((item,index) => {
+            return (
+              <li key={index}>
+                <div className="content">
+                  <i className="las la-otter" />
+                  <div>
+                    <h1>{item.temp}</h1>
+                    <h2>timeStamp:{item.timestamp}-co2:{item.co2}-hum:{item.hum}-light:{item.light}</h2>
+                  </div>
+                </div>
+              </li>
+            )
+          }
+          )}
+
+
+      </ul>
+
+
+
+
+
     </div>
   );
 }
